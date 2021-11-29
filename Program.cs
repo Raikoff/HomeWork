37695e045039ea9e1100f7890f68768b41a35723
Raikoff <podchernin.ivan@gmail.com>
2021-11-29 19:15:06 +0300

    add джун

--- Program.cs
+++ Program.cs
@@ -112,7 +112,7 @@ else if (x < 0)
 //6. Выяснить является ли число чётным
 
 int value = 6;
-int balance = 0;
+
 if(value % 2 == 0)
 {
 Console.WriteLine("Число четное");
@@ -122,26 +122,278 @@ else
     Console.WriteLine("Число не четное");
 }
 
-
-
 //7. Показать числа от -N до N
+
+int num1 = -5;
+while (num1 <= 5)
+{
+    num1++;
+}
+Console.WriteLine(num1);
+
+
+
 //8. Показать четные числа от 1 до N
+
+
 // 9. Показать последнюю цифру трёхзначного числа
+
+
+
 //10. Показать вторую цифру трёхзначного числа
+
+int number = 121;
+
+int g = number / 10;
+Console.WriteLine(g);
+int h = g % 10;
+Console.WriteLine(h);
+
+
 //11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number11 = 46;
+
+int p = number11 / 10;
+Console.WriteLine(p);
+
 //12. Удалить вторую цифру трёхзначного числа
+
+int number;
+
+Console.WriteLine("Введите 3х значное число");
+
+number = int.Parse(Console.ReadLine());
+
+if()
+
 //13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
 //14. Найти третью цифру числа или сообщить, что её нет
 
 //## Почувствуй себя джуном*
+
+
 //15. Дано число. Проверить кратно ли оно 7 и 23
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 7;
+int num2 = 23;
+int g = (num1 % num) & (num2 % num);
+if (g >= 1)
+{
+    Console.WriteLine("Не кратно");
+}
+else
+{
+    Console.WriteLine("Кратно");
+}
+
 //16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+
+Console.Write("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num == 6)
+{
+    Console.WriteLine("Суббота, выходной!!!");
+}
+else if(num == 7)
+{
+    Console.WriteLine("Воскресенье, выходной!!!");
+}
+else if(num != 6)
+{
+    Console.WriteLine("Пора работать!!!");
+}
+
 //17. По двум заданным числам проверять является ли одно квадратом другого
+
+
+Console.Write("Введите первое число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+int num1 = int.Parse(Console.ReadLine());
+int resul = num1;
+
+if(resul == num * num)
+{
+Console.WriteLine(num1 + " является квадратом " + num );
+}
+else if(resul != num * num)
+{
+    Console.WriteLine(num1 + " не является квадратом " + num);
+}
+
+
+
 //18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+Console.Write("Введите значение X true или false: ");
+bool x = bool.Parse(Console.ReadLine());
+
+Console.Write("Введите значение Y true или false: ");
+bool y = bool.Parse(Console.ReadLine());
+
+bool a = !(x || y);
+
+  Console.WriteLine(a);
+
+bool b = !x ^ !y;
+
+Console.WriteLine(b);
+
 //19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+Console.Write("Введите X: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine());
+if((x == 0) && (y == 0 ))
+{
+    Console.WriteLine("X и Y  не могут быть равны 0, введите другие значения");
+}
+
+if((x > 0) && (y > 0))
+{
+    Console.WriteLine("Точка находиться в I четверти");
+}
+else if((x > 0) && (y < 0))
+{
+    Console.WriteLine("Точка находиться в II четверти");
+}
+else if((x < 0) && (y < 0))
+{
+    Console.WriteLine("Точка находиться в III четверти");
+}
+
+else if((x > 0) && (y < 0))
+{
+    Console.WriteLine("Точка находиться в IV четверти");
+}
+
 //20. Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("Введите номер четверти: ");
+int result = int.Parse(Console.ReadLine());
+
+if(result == 1)
+{
+    Console.WriteLine("x>0;y>0");
+}
+else if(result == 2)
+{
+    Console.WriteLine("x<0;y>0");
+}
+else if(result == 3)
+{
+    Console.WriteLine("x<0;y<0");
+}
+
+else if(result == 4)
+{
+    Console.WriteLine("x>0;y<0");
+}
+if (result > 4)
+{
+Console.WriteLine("Такой четверти нет");
+}
+if (result < 1)
+{
+    Console.WriteLine("Такой четверти нет");
+}
+
 //21. Программа проверяет пятизначное число на палиндромом.
+
+
+Console.Write("Введите пятизначное число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num <= 9999 || num >= 999999)
+{
+Console.WriteLine("Не умеешь считать до пяти?");
+}
+else if ((" + num[0]" == "" + num[5]) && (""+ num[1] == num[3]))
+{
+    Console.WriteLine("Число проверяеться на полидромом");
+}
+    
+
+
 //22. Найти расстояние между точками в пространстве 2D/3D
+
+char version;
+ Double x1, x2, y1, y2, z1, z2, rasst; // вводим переменные
+
+ Console.WriteLine("1. Найти расстояние между точками в пространстве 2D");
+ Console.WriteLine("2. найти расстояние между точками в пространстве 3D");
+ Console.WriteLine("Выбирите пункт 1 или 2: ");
+
+
+ version = Convert.ToChar(Console.ReadLine());
+// делим решение на две части 2D и 3D
+ switch (version)    
+   {
+      
+      case '1':
+
+     Console.Write("Введите X1: ");
+     x1 = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("Введите Y1: ");
+     y1 = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("Введите X2: ");
+     x2 = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("Введите Y2: ");
+     y2 = Convert.ToDouble(Console.ReadLine());
+
+     rasst = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)); //  формула раасчета
+    
+     Console.WriteLine("Растояние: " + rasst);
+     Console.ReadKey(); // возвращение в терминал
+     break;
+
+     case '2': 
+
+     Console.Write("Введите X1: ");
+     x1 = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("Введите Y1: ");
+     y1 = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("Введите Z1: ");
+     z1 = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("Введите X2: ");
+     x2 = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("Введите Y2: ");
+     y2 = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("Введите Z2: ");
+     z2 = Convert.ToDouble(Console.ReadLine());
+
+     rasst = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+     Console.WriteLine("Результат" + rasst);
+     Console.ReadKey();
+     break;
+ 
+     default:
+     Console.WriteLine("Неверное значение");
+     Console.ReadKey();
+     break;
+
+    }
 
 // ## Почувствуй себя мидлом*
 //23. Показать таблицу квадратов чисел от 1 до N 
